26eff290bde9a3488d820b643ac0e9446733500f
Leo <poussin6494@hotmail.com>
2019-09-26 22:24:43 +0200

    initialisation projet

---
+++ database/migrations/2019_09_26_195631_create_dossiers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDossiersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dossiers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('d_nb_adultes');
+            $table->integer('d_nb_enfants');
+            $table->string('d_code');
+            $table->unsignedBigInteger('d_agence_id')->nullable();
+            $table->foreign('d_agence_id')->references('ag_id')->on('agences')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dossiers');
+    }
+}
