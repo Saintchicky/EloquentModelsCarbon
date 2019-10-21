4095ce19d30d777a07b012cffc6343b9e3476c0b
Leo <poussin6494@hotmail.com>
2019-10-21 20:12:20 +0200

    Catalina bdd

---
+++ app/data/20191021_patch_users.sql
@@ -0,0 +1,3 @@
+insert Into Début;
+insert into `users_settings` (`us_created_at`, `us_profil_id`, `us_updated_at`, `us_user_id`) values ('2019-10-21 20:01:29', '1', '2019-10-21 20:01:29', '1');
+insert Into Fin;
